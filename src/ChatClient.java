48d47bc01f4a7bfdf5933fc6b82dd8784f136ae2
Ethan Barlow <ren.o.amer.2016@hotmail.com>
2018-03-30 00:03:03 -0400

    Currently mimics the input of the user.

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,3 +1,7 @@
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 /*
 This is the ChatClient class that will be run by the user and will be the interface through which messages are sent
  */
@@ -5,6 +9,47 @@ public class ChatClient
 {
     public static void main(String[] a)
     {
+        try
+        {
+            //the socket object that will connect to the server
+            Socket server = new Socket("148.137.223.190", 4336);
+            //sends messages out to the server, use true as a parameter to make sure that the stream auto-flushes
+            PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+            //receives messages from the server
+            Scanner input = new Scanner(server.getInputStream());
 
+            String message="";
+
+            //used for keyboard input
+            Scanner keyboard = new Scanner(System.in);
+
+            while(keyboard.hasNextLine())
+            {
+                message = keyboard.nextLine();
+
+                out.println(message);
+                System.out.println("Your message echoed back by the server: " + input.nextLine());
+            }
+
+            /*while(true)
+            {
+                System.out.println("From the server: "+input.nextLine());
+
+                if(keyboard.hasNextLine())
+                {
+                    message = keyboard.nextLine();
+
+                    //send the message to the server
+                    out.println(message);
+                    //get a response back from the server
+                }
+            }*/
+
+           // System.out.println("Connected to server: " + server.getInetAddress());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,22 +1,57 @@
 /*
-This is the chat server class that will coordinate all the message traffic
+This is the chat server class that will setup all connections with clients
 
 This class needs the following attributes:
     ServerSocket - to listen for new connections
-    Map<String username, Socket client>
+    Map<String username, ClientThread client>
 
 
 
  */
 
+import com.sun.deploy.util.SessionState;
+
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatServer
 {
+
     //this main method will run the ChatServer
     public static void main(String[] a)
     {
-        Socket s = new Socket();
+        Map<String, ClientThread> userMap = new HashMap<>(); // will hold the usernames as the key, and the ClientThread object as the value
+
+        try
+        {
+            ServerSocket s = new ServerSocket(4336);
+            while(true)
+            {
+                Socket client = s.accept();
+                System.out.println("Connected: " + client.getInetAddress());
+                new ClientThread(client);
+                /*Socket client = s.accept();//continuously waiting for a connection
+                System.out.println("Connected: " + client.getInetAddress());
+
+                ClientThread ct = new ClientThread(client, userMap.keySet());
+                //add the ClientThread object to the map after processing out the username
+                System.out.println("Username --- "+ct.getUserName());
+                userMap.put(ct.getUserName(), ct);
+                for(String str: userMap.keySet())
+                    System.out.println(str);
+                ct.start();*/
+            }
+
+
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 
     }
 }
